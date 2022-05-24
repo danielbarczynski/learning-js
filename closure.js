91d1ee7772a462073409511544ec917d063ced9e
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-05-24 13:55:39 +0200

    update

--- closure.js
+++ closure.js
@@ -1,7 +1,7 @@
 function init() {
   var name = "Mozilla"; // name jest zmienną lokalną utworzoną przez funkcję init
   function displayName() {
-    // displayName() jest wewnętrzną funkcją, domknięciem
+    // displayName() jest wewnętrzną funkcją, domknięciem, ma dostep do name variable
     alert(name); // używa zmiennej zdeklarowanej w funkcji nadrzędnej
   }
   displayName();
@@ -20,4 +20,18 @@ var add10 = makeAdder(10);
 console.log(add5(2)); // 7
 console.log(add10(2)); // 12
 
-//add5 i add10 są domknięciami. Dzielą ten sam kod zawarty w funkcji makeAdder, ale przechowują różne leksykalne środowisko. W leksykalnym środowisku add5, x wynosi 5, natomiast w leksykalnym środowisku add10, x jest równe 10.
+//add5 i add10 są domknięciami. Dzielą ten sam kod zawarty w funkcji makeAdder, ale przechowują różne leksykalne środowisko. 
+//W leksykalnym środowisku add5, x wynosi 5, natomiast w leksykalnym środowisku add10, x jest równe 10.
+
+
+// other example with html/css
+
+function makeSizer(size) {
+  return function() {
+    document.body.style.fontSize = size + 'px';
+  };
+}
+
+var size12 = makeSizer(12);
+var size14 = makeSizer(14);
+var size16 = makeSizer(16);
