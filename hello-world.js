ecf53898d974c25eb9ffc396978931f9ef858437
Daniel PC <daniel.barczynski@onet.pl>
2022-11-09 14:39:27 +0100

    update

--- hello-world.js
+++ hello-world.js
@@ -40,8 +40,47 @@ let eq = 3 == 3 ? true : false;
 let eq2 = 3 == '3' ? true : false;
 let eq3 = 3 === 3 ? true : false;
 let eq4 = 3 === '3' ? true : false;
+let eq5 = 3 != '3' ? true : false;
+let eq6 = 3 !== '3' ? true : false;
 
 console.log(eq);
 console.log(eq2);
 console.log(eq3);
 console.log(eq4);
+console.log(eq5);
+console.log(eq6);
+
+//Switch
+const rateValue = (value) => {
+    if(value < 0)
+        return;
+
+    switch (value) {
+        case 1:
+        case 2:
+        case 3:
+            return 'low'; // break not neccessary while returning
+        case 5:
+            console.log('mid');
+            break;
+        default:
+            return 'high';
+    }
+}
+
+console.log(rateValue(2));
+console.log(rateValue(20));
+console.log(rateValue(-20));
+rateValue(5);
+
+// Objects
+const dog = {
+    name: 'Miky',
+    age: 10
+}
+
+dog['canSwim'] = false;
+dog.canBark = true;
+delete dog.age;
+
+console.log(dog);
