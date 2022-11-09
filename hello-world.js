ab589c43eb90a8193e5ff5b2dfb9ac391ead1f70
Daniel PC <daniel.barczynski@onet.pl>
2022-11-09 13:44:49 +0100

    update

--- hello-world.js
+++ hello-world.js
@@ -2,3 +2,17 @@ console.log('hello world'); // node .
 console.log(`hello
 world
 multiline`);
+
+// Nested Array
+const nestedArray = [[4, 'j'], [5, 'g']];
+let myData = nestedArray[1][1];
+console.log(myData);
+nestedArray.push([9, 9]); // add last element
+nestedArray.pop(); // remove last element
+nestedArray.shift(); // remove first element
+nestedArray.unshift([1, 1]); // add first element
+console.log(nestedArray);
+
+// Parameters & Arguments
+const arrowF = (a, b) => console.log([a, b]); // parameters
+arrowF(1, 2); // arguments
