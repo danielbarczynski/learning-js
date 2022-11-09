0b92f0ab7b35f5ba0619b2ce9f926bf1a799fa5c
Daniel PC <daniel.barczynski@onet.pl>
2022-11-09 14:01:07 +0100

    update

--- hello-world.js
+++ hello-world.js
@@ -3,7 +3,7 @@ console.log(`hello
 world
 multiline`);
 
-// Nested Array
+//* Nested Array
 const nestedArray = [[4, 'j'], [5, 'g']];
 let myData = nestedArray[1][1];
 console.log(myData);
@@ -13,6 +13,35 @@ nestedArray.shift(); // remove first element
 nestedArray.unshift([1, 1]); // add first element
 console.log(nestedArray);
 
-// Parameters & Arguments
+//* Parameters & Arguments
 const arrowF = (a, b) => console.log([a, b]); // parameters
 arrowF(1, 2); // arguments
+
+//* Scope
+function scope() {
+    var myV = 5;
+    globall = 6;
+    console.log(myV);
+}
+// console.log(globall); // error, not defined
+// console.log(myV); // error (even for var)
+
+//* Assigning returned value
+const assign = (x, y) => { // must be with curly brackets because of return
+    return x + y;
+}
+
+let assigned = assign(5, 5);
+console.log(assigned);
+
+//* Equals
+
+let eq = 3 == 3 ? true : false;
+let eq2 = 3 == '3' ? true : false;
+let eq3 = 3 === 3 ? true : false;
+let eq4 = 3 === '3' ? true : false;
+
+console.log(eq);
+console.log(eq2);
+console.log(eq3);
+console.log(eq4);
