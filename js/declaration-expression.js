9a992879b861e8e4ac73fb40e41ff94ba807386b
Daniel PC <daniel.barczynski@onet.pl>
2023-01-04 17:36:13 +0100

    up

--- js/declaration-expression.js
+++ js/declaration-expression.js
@@ -10,12 +10,18 @@ function add(number) {
 // anonymous function 
 () => console.log('hello anonymous function'); // won't print
 (() => console.log('hello anonymous function'))(); // will print
+// (() => substract(6))(); // cannot access before initialization
+(() => console.log(add(5)))();
 
 // function expression
 const substract = function (number) {
     console.log(number);
     return number;
 }
+
+substract(6);
+(() => console.log(substract(6)))(); 
+
 const substract2 = function () {
     substract(2);
     console.log('expression');
