62196f253b6553da5faca863cc0fdfa994639ebd
Daniel Barczyński <daniel.barczynski@onet.pl>
2023-01-04 16:44:11 +0100

    up

--- js/declaration-expression.js
+++ js/declaration-expression.js
@@ -1,17 +1,21 @@
 //before init
 console.log(add(4));
 // console.log(substract(5)); // returns error before init because it is treated like a variable
+//* In short, use function declarations when you want to create a function on the global scope and make it available throughout your code. 
+//* Use function expressions to limit where the function is available, keep your global scope light, and maintain clean syntax.
 
 // function declaration, you cannot pass to another function
+// Function declarations load before any code is executed while Function expressions load only when the interpreter reaches that line of code.
 function add(number) {
     return number;
 }
 
 // anonymous function 
-() => console.log('hello anonymous function'); // won't print
-(() => console.log('hello anonymous function'))(); // will print
-
-// function expression
+//* (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined
+() => console.log('hello IIFE'); // won't print
+(() => console.log('hello IIFE'))(); // will print
+(function() {console.log('hello IIFE')})(); // will print
+// function expression (can be use as an IIFE)
 const substract = function (number) {
     console.log(number);
     return number;
